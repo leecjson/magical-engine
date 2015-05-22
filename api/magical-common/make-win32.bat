398f12cf1f6a40995e0d5836e43818f40e8c9d1b
Jason.lee <jason.lee.c@foxmail.com>
2015-05-23 00:13:39 +0800

    go

--- api/magical-common/make-win32.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0../tolua++
-toluapp-win32 -n common -L toluapp-basic.lua -o ../../source/magical-engine/src/magical-lua/binding/BindCommon.cpp ../magical-common/Include.pkg
-pause
--- source/demo/assets/standard/shaders/glsl/test_vertex.glsl
+++
@@ -1,7 +0,0 @@
-attribute vec4 a_vertex;
-uniform mat4 u_mvp_matrix;
-
-void main( void )
-{
-    gl_Position = u_mvp_matrix * a_vertex;
-}
